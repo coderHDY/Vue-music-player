14f72a47fd9636ec33582a027c71c35239de4c67
coderHDY <huangdeyu2020@163.com>
2022-04-01 18:03:59 +0800

    pwa

--- vue.config.js
+++ vue.config.js
@@ -21,5 +21,15 @@ module.exports = {
         }
       }
     }
+  },
+  pwa: {
+    workboxOptions: {
+      skipWaiting: true,
+      clientsClaim: true,
+      importWorkboxFrom: 'local',
+      importsDirectory: 'js',
+      navigateFallback: '/',
+      navigateFallbackBlacklist: [/\/api\//],
+    }
   }
 }
