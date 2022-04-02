2b9fd44e6af972144e59c8d84e7a9faf5c9ed76a
coderHDY <huangdeyu2020@163.com>
2022-04-02 08:46:37 +0800

    推荐样式调整

--- src/components/common/tabbar/TabBar.vue
+++ src/components/common/tabbar/TabBar.vue
@@ -1,28 +1,28 @@
 <template>
-  <div class = "tab-bar">
+  <div class="tab-bar">
     <slot></slot>
   </div>
 </template>
 <script>
-  //流式底部Bar，类似微信下面的布局,最后一个文字div可传点击颜色  <div :activeColor="" slot="item-name">{{}}<div>
-  //参照 tab-bar>tab-bar-item[:link=]{$}>img[slot="item-icon"]+img[slot="item-icon-active"]+div[slot="item-name"]{\{{}}}
-  export default {
-    name: "TabBar"
-  }
+//流式底部Bar，类似微信下面的布局,最后一个文字div可传点击颜色  <div :activeColor="" slot="item-name">{{}}<div>
+//参照 tab-bar>tab-bar-item[:link=]{$}>img[slot="item-icon"]+img[slot="item-icon-active"]+div[slot="item-name"]{\{{}}}
+export default {
+  name: "TabBar"
+};
 </script>
 
 <style scoped>
-  .tab-bar {
-    display: flex;
-    background-color: #f3f3f3;
-    position: fixed;
-    height: 60px;
-    left: 0;
-    right: 0;
-    bottom: 0;
-    box-shadow: 0 -2px 2px rgba(130, 130, 200, .2);
-    user-select: none;
-    overflow: hidden;
-    z-index: 6;
-  }
+.tab-bar {
+  display: flex;
+  background-color: #f3f3f3;
+  position: fixed;
+  height: 75px;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  box-shadow: 0 -2px 2px rgba(130, 130, 200, 0.2);
+  user-select: none;
+  overflow: hidden;
+  z-index: 6;
+}
 </style>
--- src/views/home/childcomps/RecommendList.vue
+++ src/views/home/childcomps/RecommendList.vue
@@ -1,14 +1,18 @@
 <template>
-  <div class = "recommend-box">
-    <div class = "recommend-title">
+  <div class="recommend-box">
+    <div class="recommend-title">
       推荐歌单
     </div>
-    <div class = "scroll-wrapper" ref = "scrollWrapper">
-      <div class = "recommend-list scroll-content">
-        <div class = "recommend-item" v-for = "item in songLists" @click = "queryList(item.name,item.id)">
-          <img :src = "item.img" class = "item-img" @load = "imageLoad">
-          <div class = "item-name">{{item.name}}</div>
-          <div class = "item-count">{{item.count | count}}</div>
+    <div class="scroll-wrapper" ref="scrollWrapper">
+      <div class="recommend-list scroll-content">
+        <div
+          class="recommend-item"
+          v-for="item in songLists"
+          @click="queryList(item.name, item.id)"
+        >
+          <img :src="item.img" class="item-img" @load="imageLoad" />
+          <div class="item-name">{{ item.name }}</div>
+          <div class="item-count">{{ item.count | count }}</div>
         </div>
       </div>
     </div>
@@ -16,133 +20,136 @@
 </template>
 
 <script>
-  import { initHomeSongList } from "../../../network/types";
-  import BScroll from "better-scroll"
-  import ObserveImage from "@better-scroll/observe-image"
+import { initHomeSongList } from "../../../network/types";
+import BScroll from "better-scroll";
+import ObserveImage from "@better-scroll/observe-image";
 
-  export default {
-    name: "RecommendList",
-    props: {
-      recommendSongList: {
-        type: Array,
-        default: null
+export default {
+  name: "RecommendList",
+  props: {
+    recommendSongList: {
+      type: Array,
+      default: null
+    }
+  },
+  data() {
+    return {
+      Bscroll: null,
+      refresh: null,
+      isEmit: false
+    };
+  },
+  computed: {
+    songLists() {
+      return initHomeSongList(this.recommendSongList) || [];
+    }
+  },
+  methods: {
+    imageLoad() {
+      if (!this.isEmit) {
+        this.$emit("imageLoad");
+        this.isEmit = true;
       }
     },
-    data() {
-      return {
-        Bscroll: null,
-        refresh: null,
-        isEmit: false
-      }
+    initBS() {
+      BScroll.use(ObserveImage);
+      const bs = new BScroll(this.$refs.scrollWrapper, {
+        scrollX: true,
+        scrollY: false,
+        click: true,
+        probeType: 1,
+        observeImage: true
+      });
+      this.Bscroll = bs;
     },
-    computed: {
-      songLists() {
-        return initHomeSongList(this.recommendSongList) || []
-      }
-    },
-    methods: {
-      imageLoad() {
-        if (!this.isEmit) {
-          this.$emit("imageLoad");
-          this.isEmit = true
-        }
-      },
-      initBS() {
-        BScroll.use(ObserveImage);
-        const bs = new BScroll(this.$refs.scrollWrapper, {
-          scrollX: true,
-          scrollY: false,
-          click: true,
-          probeType: 1,
-          observeImage: true
-        });
-        this.Bscroll = bs;
-      },
-      queryList(name, id) {
-        const option = {
-          type: "queryList",
-          name: name,
-          id: id
-        };
-        this.$emit("queryList", option)
-      }
-    },
-    mounted() {
-      this.initBS()
-    },
-    filters: {
-      count(count) {
-        return (count / 10000).toFixed(0) + "万"
-      }
+    queryList(name, id) {
+      const option = {
+        type: "queryList",
+        name: name,
+        id: id
+      };
+      this.$emit("queryList", option);
+    }
+  },
+  mounted() {
+    this.initBS();
+  },
+  filters: {
+    count(count) {
+      let ans = count / 10000;
+      return ans > 10000
+        ? Math.floor(ans / 10000) + "亿"
+        : Math.floor(ans) + "万";
     }
   }
+};
 </script>
 
 <style scoped>
-  .recommend-box {
-    position: relative;
-    background-color: #fff;
-    margin-top: .3rem;
-    margin-bottom: 10px;
-  }
+.recommend-box {
+  position: relative;
+  background-color: #fff;
+  margin-top: 0.3rem;
+  margin-bottom: 10px;
+}
 
-  .recommend-title {
-    height: 2rem;
-    line-height: 2rem;
-    font-size: var(--font-big);
-    padding-left: 1rem;
-    font-weight: bolder;
-  }
+.recommend-title {
+  height: 2rem;
+  line-height: 2rem;
+  font-size: var(--font-big);
+  padding-left: 1rem;
+  font-weight: bolder;
+}
 
-  .recommend-list {
-    position: relative;
-    height: 26vh;
-    padding-top: .1rem;
-    padding-bottom: .1rem;
-    white-space: nowrap;
-  }
+.recommend-list {
+  position: relative;
+  height: 26vh;
+  padding-top: 0.1rem;
+  padding-bottom: 0.1rem;
+  white-space: nowrap;
+}
 
-  .scroll-content {
-    display: inline-block;
-    padding-left: 10px;
-  }
+.scroll-content {
+  display: inline-block;
+  padding-left: 10px;
+}
 
-  .recommend-item {
-    position: relative;
-    display: inline-block;
-    width: 23vh;
-    height: 100%;
-    padding: .1rem .1rem;
-    text-align: center;
-  }
+.recommend-item {
+  position: relative;
+  display: inline-block;
+  width: 23vh;
+  height: 100%;
+  padding: 0.1rem 0.1rem;
+  text-align: top;
+}
 
-  .recommend-item img {
-    width: 80%;
-    border-radius: .5rem;
-    vertical-align: middle;
-  }
+.recommend-item img {
+  width: 80%;
+  border-radius: 0.5rem;
+  vertical-align: top;
+}
 
-  .item-name {
-    position: relative;
-    display: block;
-    margin: 3px 7%;
-    height: 3.5rem;
-    font-size: var(--font-small);
-    line-height: 16px;
-    text-align: left;
-    overflow: hidden;
-    white-space: normal;
-    word-wrap: break-word;
-  }
+.item-name {
+  position: relative;
+  display: block;
+  margin: 3px 7%;
+  height: 3.5rem;
+  font-size: var(--font-small);
+  line-height: 16px;
+  text-align: left;
+  overflow: hidden;
+  white-space: normal;
+  word-wrap: break-word;
+}
 
-  .item-count {
-    position: absolute;
-    right: 3.5vh;
-    top: 4px;
-    padding: 1px 3px;
-    border-radius: .3rem;
-    font-size: .3rem;
-    color: #fff;
-    background-color: rgba(181, 181, 181, 0.7);
-  }
+.item-count {
+  position: absolute;
+  right: 21%;
+  top: 4px;
+  padding: 1px 3px;
+  border-radius: 0.3rem;
+  font-size: 0.3rem;
+  color: #fff;
+  background-color: rgba(181, 181, 181, 0.7);
+}
 </style>
