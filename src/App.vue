67ce28366f1cd794c1ed2f2da7d15016c8869062
coderHDY <huangdeyu2020@163.com>
2022-04-02 16:58:13 +0800

    主页缓存

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,25 @@
 <template>
-  <div id = "app">
-    <keep-alive exclude = "SongsList,Home">
-      <router-view/>
+  <div id="app">
+    <keep-alive exclude="SongsList">
+      <router-view />
     </keep-alive>
-    <MainTabBar/>
-    <Player/>
+    <MainTabBar />
+    <Player />
   </div>
 </template>
 
 <script>
-  import MainTabBar from "./components/content/maintabbar/MainTabBar";
+import MainTabBar from "./components/content/maintabbar/MainTabBar";
 import Player from "./views/pleyer/Player";
-  export default {
-    name: "app",
-    components: {
-      MainTabBar,
-      Player
-    }
+export default {
+  name: "app",
+  components: {
+    MainTabBar,
+    Player
   }
+};
 </script>
 
 <style>
-  @import "assets/css/baseConfig/base.css";
+@import "assets/css/baseConfig/base.css";
 </style>
